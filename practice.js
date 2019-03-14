0c1a194248f2494d0560115b6aa1d9655a9bea75
detox2k2 <39745373+detox2k2@users.noreply.github.com>
2019-03-13 21:42:00 -0500

    Add files via upload

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0]
+};
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1]
+};
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(fam){
+for (let i =0; i < fam.length;i++){
+  alert(fam[i])
+}
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for (let i=0; i > letters.length;i++){
+    alert(letters[i])
+  }
+};
 
 
 
@@ -78,6 +91,12 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+function eventfinder(nums){
+for (let i=0; i < nums.even;i++){
+ alert(nums)
+}
+};
 
 
 
