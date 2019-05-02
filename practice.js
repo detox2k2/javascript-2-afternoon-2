831997520f36f4eb00799938b22c553628e5bf73
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-05-01 22:56:23 -0500

    afternoon project done

--- practice.js
+++ practice.js
@@ -18,10 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-function first(arr){
-  return arr[0]
-};
-
+function first (arr){
+return (arr[0])
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -35,9 +34,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-function last(arr){
-  return arr[arr.length-1]
-};
+function last (arr){
+return(arr[2])
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -52,11 +51,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-function looper(fam){
-for (let i =0; i < fam.length;i++){
-  alert(fam[i])
+function looper(family){
+for (var i =0; i < family.length; i++)
+alert(family[i])
 }
-};
 
 
 ////////// PROBLEM 4 //////////
@@ -72,12 +70,10 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-function reversedLooper(letters){
-  for (var i = letters.length-1; i >= 0; i--){
-    alert(letters[i])
-  }
+function reversedLooper (letters){
+for ( var i = letters.length-1; i >= 0; i--)
+alert(letters[i])
 }
- reversedLooper(letters);
 
 
 
@@ -92,16 +88,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
   Write a function named evenFinder that is given nums as it's only argument.
   Return an array that contains the even numbers from the nums array.
 */
-
+[1,2,3,4,5,6]
 //Code Here
-
-var evenFinder = (nums) => {
-  var result = [];
+function evenFinder (nums) {
+  let evens = []
   for (var i = 0; i < nums.length; i++) {
-    if (nums[i] % 2 === 0)
-    result.push(nums[i]);
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i])
+    }
   }
-  return result;
+  return evens
 }
 
 
